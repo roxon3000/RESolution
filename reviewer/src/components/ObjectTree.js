284d57200fc309d3b50a107b0bb19962334db12b
Roxon3000 <jgooder@protonmail.com>
2022-12-19 10:56:02 -0500

    switching to hooks and functional components

--- reviewer/src/components/ObjectTree.js
+++ reviewer/src/components/ObjectTree.js
@@ -1,110 +1,75 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { getObjectTree } from '../services';
-import { connect } from 'react-redux';
+import { connect, useDispatch, useSelector } from 'react-redux';
 import Template from './Template'
 import { Classes, Icon, Intent, Tree, TreeNodeInfo } from "@blueprintjs/core";
+import { useParams } from "react-router-dom"
 
 // example = https://github.com/palantir/blueprint/blob/develop/packages/docs-app/src/examples/core-examples/treeExample.tsx
-// 
+//
 
-class ObjectTree extends React.Component {
+function ObjectTree(props) {
 
-    constructor(props) {
-        super(props)
-        this.props.loadObjTreeData()
-    }
-    /*
-    handleNodeClick = React.useCallback(
-        (node, nodePath, e) => {
-            const originallySelected = node.isSelected;
-            if (!e.shiftKey) {
-                dispatch({ type: "DESELECT_ALL" });
-            }
-            dispatch({
-                payload: { path: nodePath, isSelected: originallySelected == null ? true : !originallySelected },
-                type: "SET_IS_SELECTED",
-            });
-        },
-        [],
+    const dispatch = useDispatch();
+    var nodes = useSelector((state) => state.objecttree.tree);
+    var loading = useSelector(
+        (state) => state.objecttree.loading
     );
 
-    handleNodeCollapse = React.useCallback((_node, nodePath) => {
-        dispatch({
-            payload: { path: nodePath, isExpanded: false },
-            type: "SET_IS_EXPANDED",
-        });
-    }, []);
+    useEffect(
 
-    handleNodeExpand = React.useCallback((_node, nodePath) => {
+        () => {
+            if (loading) {
+                dispatch(getObjectTree());
+            }
+        }
+
+    );
+
+    const handleNodeExpand = (node, nodePath) => {
+
         dispatch({
             payload: { path: nodePath, isExpanded: true },
             type: "SET_IS_EXPANDED",
         });
-    }, []);
-    */
+    }; 
 
-    render() {
-        var nodes = this.props.tree
+    const handleNodeClick = (node, nodePath, e) => {
+        const originallySelected = node.isSelected;
 
-        return (
-            <Template>
-                <div> Object Tree - add something here</div>
-                <div>
-                    
-                </div>
-                <Tree
-                    contents={nodes}
-                    onNodeClick={this.props.handleNodeClick}
-                    onNodeCollapse={this.props.handleNodeCollapse}
-                    onNodeExpand={this.props.handleNodeExpand}
-                    className={Classes.ELEVATION_0}
-                />
-
-                
-            </Template>
-
-        );
-    }
-}
-
-
-const mapDispatchToProps = dispatch => {
-    return {
-        loadObjTreeData: () => {
-            dispatch(getObjectTree());
-        },
-        handleNodeExpand: (node, nodePath) => {
-            dispatch({
-                payload: { path: nodePath, isExpanded: true },
-                type: "SET_IS_EXPANDED",
-            });
-        },
-        handleNodeClick: (node, nodePath, e) => {
-            const originallySelected = node.isSelected;
-            if (!e.shiftKey) {
-                dispatch({ type: "DESELECT_ALL" });
-            }
-            dispatch({
-                payload: { path: nodePath, isSelected: originallySelected == null ? true : !originallySelected },
-                type: "SET_IS_SELECTED",
-            });
-        },
-        handleNodeCollapse: (_node, nodePath) => {
-            dispatch({
-                payload: { path: nodePath, isExpanded: false },
-                type: "SET_IS_EXPANDED",
-            });
+        if (!e.shiftKey) {
+            dispatch({ type: "DESELECT_ALL" });
         }
-    }
+        dispatch({
+            payload: { path: nodePath, isSelected: originallySelected == null ? true : !originallySelected },
+            type: "SET_IS_SELECTED",
+        })
+    };
+
+    const handleNodeCollapse = (_node, nodePath) => {
+        dispatch({
+            payload: { path: nodePath, isExpanded: false },
+            type: "SET_IS_EXPANDED",
+        });
+    };
+    return (
+        <Template>
+            <div> Object Tree - add something here</div>
+            <div>
+
+            </div>
+            <Tree
+                contents={nodes}
+                onNodeClick={handleNodeClick}
+                onNodeCollapse={handleNodeCollapse}
+                onNodeExpand={handleNodeExpand}
+                className={Classes.ELEVATION_0}
+            />
+
+
+        </Template>
+
+    );
 }
 
-const mapStateToProps = state =>
-(
-    {
-        tree: state.objecttree.tree,
-        loading: state
-    }
-)
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(ObjectTree);
+export default ObjectTree;
--- reviewer/src/redux/reducers/objecttree.js
+++ reviewer/src/redux/reducers/objecttree.js
@@ -22,86 +22,8 @@ const INITIAL_STATE = {
     loading: true,
     raw : null
 
-    }
-const INITIAL_STATE_example =
-[
-    {
-        id: 0,
-        hasCaret: true,
-        icon: "folder-close",
-        label: (
-            <ContextMenu2 {...contentSizing} content={<div>Hello there!</div>}>
-                Folder 0
-            </ContextMenu2>
-        ),
-    },
-    {
-        id: 1,
-        icon: "folder-close",
-        isExpanded: true,
-        label: (
-            <ContextMenu2 {...contentSizing} content={<div>Hello there!</div>}>
-                <Tooltip2 content="I'm a folder <3" placement="right">
-                    Folder 1
-                </Tooltip2>
-            </ContextMenu2>
-        ),
-        childNodes: [
-            {
-                id: 2,
-                icon: "document",
-                label: "Item 0",
-                secondaryLabel: (
-                    <Tooltip2 content="An eye!">
-                        <Icon icon="eye-open" />
-                    </Tooltip2>
-                ),
-            },
-            {
-                id: 3,
-                icon: <Icon icon="tag" intent={Intent.PRIMARY} className={Classes.TREE_NODE_ICON} />,
-                label: "Organic meditation gluten-free, sriracha VHS drinking vinegar beard man.",
-            },
-            {
-                id: 4,
-                hasCaret: true,
-                icon: "folder-close",
-                label: (
-                    <ContextMenu2 {...contentSizing} content={<div>Hello there!</div>}>
-                        <Tooltip2 content="foo" placement="right">
-                            Folder 2
-                        </Tooltip2>
-                    </ContextMenu2>
-                ),
-                childNodes: [
-                    { id: 5, label: "No-Icon Item" },
-                    { id: 6, icon: "tag", label: "Item 1" },
-                    {
-                        id: 7,
-                        hasCaret: true,
-                        icon: "folder-close",
-                        label: (
-                            <ContextMenu2 {...contentSizing} content={<div>Hello there!</div>}>
-                                Folder 3
-                            </ContextMenu2>
-                        ),
-                        childNodes: [
-                            { id: 8, icon: "document", label: "Item 0" },
-                            { id: 9, icon: "tag", label: "Item 1" },
-                        ],
-                    },
-                ],
-            },
-        ],
-    },
-    {
-        id: 2,
-        hasCaret: true,
-        icon: "folder-close",
-        label: "Super secret files",
-        disabled: true,
-    },
-    ];
+}
+
 /*
 type NodePath = number[];
 
