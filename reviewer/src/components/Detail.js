a3810b00b8a75a46f10d8689860d24b08964ad6a
Roxon3000 <jgooder@protonmail.com>
2023-01-17 09:16:29 -0500

    add file name to details comp

--- reviewer/src/components/Detail.js
+++ reviewer/src/components/Detail.js
@@ -2,11 +2,21 @@ import React, { useEffect } from 'react';
 import Template from './Template'
 import Summary from './Summary'
 import ObjectTree from './ObjectTree'
+import { useSearchParams } from "react-router-dom";
+
 function Detail(props) {
+
+   let [searchParams, setSearchParams] = useSearchParams();
+   let file = searchParams.get('file');
 
    return (
        <Template>
            <div className="container">
+               <div className="row">
+                   <div className="col">
+                       <h1>Select File: {file}</h1>
+                   </div>
+               </div>
                <div className="row">
                    <div className="col">
                        <Summary></Summary>
